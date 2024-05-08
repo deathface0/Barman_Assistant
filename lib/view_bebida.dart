2c20836628d52281416bbf0f5d0e455e0eb3a1eb
deathface0 <jorgetudela2003@gmail.com>
2024-05-08 13:04:42 +0200

    Landscape drink view fixed

--- lib/view_bebida.dart
+++ lib/view_bebida.dart
@@ -9,6 +9,100 @@ class ViewBebida extends StatelessWidget {
   ViewBebida({super.key, required this.nombre}) :
         _bebida = Bebida(id: '', nombre: nombre, urlImagen: '', instrucciones: '');
 
+  Column getOrientatedUI(BuildContext context, Orientation orientation)
+  {
+    if (orientation == Orientation.landscape)
+    {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(left: 10.0, top: 15.0), // Añadir relleno a la izquierda
+                  child: Image.network(
+                    _bebida.urlImagen,
+                    width: 300, // Ajusta el ancho según sea necesario
+                    height: MediaQuery.of(context).size.height * 0.7, // Ajusta la altura según sea necesario
+                  ),
+                ),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        _bebida.nombre,
+                        textAlign: TextAlign.justify, // Set text alignment to justify
+                        style: const TextStyle(
+                          fontSize: 20, // Adjust the font size as needed
+                          fontWeight: FontWeight.bold, // Adjust the font weight as needed
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.55,
+                        height: MediaQuery.of(context).size.height * 0.6, // Limita la altura al 30% del tamaño de la pantalla
+                        child: SingleChildScrollView(
+                          child: Text(
+                            _bebida.instrucciones,
+                            textAlign: TextAlign.justify,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                ),
+              ],
+          ),
+        ],
+      );
+    } else
+    {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(height: 40),
+          Image.network(
+            _bebida.urlImagen,
+            width: 300, // Adjust the width as needed
+            height: 300, // Adjust the height as needed
+          ),
+          const SizedBox(height: 20), // Add spacing between image and text
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0), // Set left and right padding
+            child: Text(
+              _bebida.nombre,
+              textAlign: TextAlign.justify, // Set text alignment to justify
+              style: const TextStyle(
+                fontSize: 20, // Adjust the font size as needed
+                fontWeight: FontWeight.bold, // Adjust the font weight as needed
+              ),
+            ),
+          ),
+          const SizedBox(height: 20), // Add spacing between image and text
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0), // Set left and right padding
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3, // Limita la altura al 30% del tamaño de la pantalla
+                child: SingleChildScrollView(
+                  child: Text(
+                    _bebida.instrucciones,
+                    textAlign: TextAlign.justify,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+          ),
+        ],
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final future = FutureBuilder(
@@ -21,45 +115,10 @@ class ViewBebida extends StatelessWidget {
             );
           } else if (snapshot.hasData) { // Check if data is available
             _bebida = snapshot.data;
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(height: 40),
-                Image.network(
-                  _bebida.urlImagen,
-                  width: 300, // Adjust the width as needed
-                  height: 300, // Adjust the height as needed
-                ),
-                const SizedBox(height: 20), // Add spacing between image and text
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0), // Set left and right padding
-                  child: Text(
-                    _bebida.nombre,
-                    textAlign: TextAlign.justify, // Set text alignment to justify
-                    style: const TextStyle(
-                      fontSize: 20, // Adjust the font size as needed
-                      fontWeight: FontWeight.bold, // Adjust the font weight as needed
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20), // Add spacing between image and text
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0), // Set left and right padding
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.3, // Limita la altura al 50% del tamaño de la pantalla
-                    child: SingleChildScrollView(
-                      child: Text(
-                        _bebida.instrucciones,
-                        textAlign: TextAlign.justify,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ),
-                  )
-                ),
-              ],
+            return OrientationBuilder(
+                builder: (context, orientation) {
+                  return getOrientatedUI(context, orientation);
+                },
             );
           } else {
             return const Center(child: Text('No hay datos disponibles'));
